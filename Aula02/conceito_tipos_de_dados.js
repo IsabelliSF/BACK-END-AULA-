7e046b6e9faa10adf3f10142a64dd06a19c1523f
IsabelliSF <isabellisantosfirmino@gmail.com>
2026-09-18 11:31:58 -0300

    Exercício 1 de JS

--- Aula02/conceito_tipos_de_dados.js
+++ Aula02/conceito_tipos_de_dados.js
@@ -9,3 +9,26 @@
 // Array (listas de valores)
 //  Null (valor vazio)
 // Undefined (quando algo não foi definido)
+
+
+
+// STRING (textos)
+// \uma string é um texto, sempre escrito entre aspas ("" ou '')
+let nome= "Jarvis";
+let mensagem = 'Olá, mundo!';
+
+console.log(nome);
+console.log(mensagem);
+
+let saudacao = "Olá, " + nome + "!";
+console.log(saudacao); //Exibe "Olá, Jarvis"
+
+// typeof
+// O typeof serve para descobrir o tipo de um valor ou variável
+
+let nomeDois = "Brayan - Não pode fone de ovido";
+console.log(typeof nomeDois);
+
+let soma = 10 + 5;
+console.log(soma);
+
---
+++ Aula02/exercício_01.js
@@ -0,0 +1,18 @@
+// Atividade Number
+// Crie uma variável chamada anoAtual e outras chamada anoNascimento.
+// Calcule  eexiba a idade no (a) console. 
+
+let anoAtual = "2026";
+let anoNascimento = "2010";
+let idade = anoAtual - anoNascimento;
+let mensagem1 = "Ano atual: " + anoAtual;
+let mensagem2 = "Ano de Nascimento: " + anoNascimento;
+let mensagem3 = "Idade: " + idade;
+
+console.log(anoAtual);
+console.log(anoNascimento);
+console.log(idade);
+console.log(mensagem1);
+console.log(mensagem2);
+console.log(mensagem3);
+
